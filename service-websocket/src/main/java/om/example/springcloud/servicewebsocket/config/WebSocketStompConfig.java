ce92aee809a4fd73e915bcb0b099aabc5556f2cd
赵正来 <zhaozhenglai@beadwallet.com>
2019-03-05 19:42:21 +0800

    springcloud study

--- service-websocket/src/main/java/om/example/springcloud/servicewebsocket/config/WebSocketStompConfig.java
+++ service-websocket/src/main/java/om/example/springcloud/servicewebsocket/config/WebSocketStompConfig.java
@@ -2,29 +2,41 @@ package om.example.springcloud.servicewebsocket.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 /**
  * @author 赵正来
- * @Date 2019/3/4 11:41
+ * @date 2019/3/4 11:41
  * @since jdk1.8
  */
 
 @Configuration
+@EnableScheduling
 @EnableWebSocketMessageBroker
 public class WebSocketStompConfig implements WebSocketMessageBrokerConfigurer {
+
+
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/portfolio")
-                .setAllowedOrigins("*")
+        registry.addEndpoint("/portfolio","/webSocket")
+                .setAllowedOrigins("*")//
                 .withSockJS();
     }
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic", "/queue");
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+//        registry.enableSimpleBroker("/topic", "/queue");
+        registry.enableStompBrokerRelay("/exchange","/topic","/queue","/amq/queue")
+                .setRelayHost("http://192.168.141.238")
+                .setRelayPort(61613)
+                .setClientLogin("admin")
+                .setClientPasscode("admin")
+                .setSystemHeartbeatSendInterval(5000)
+                .setSystemHeartbeatReceiveInterval(4000);
+        //registry.setApplicationDestinationPrefixes("app");
     }
 }
--- service-websocket/src/main/java/om/example/springcloud/servicewebsocket/controller/GreetingController.java
+++ service-websocket/src/main/java/om/example/springcloud/servicewebsocket/controller/GreetingController.java
@@ -1,16 +1,33 @@
 package om.example.springcloud.servicewebsocket.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author 赵正来
- * @Date 2019/3/4 13:14
+ * @date 2019/3/4 13:14
  * @since jdk1.8
  */
 @Controller
 public class GreetingController {
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+
+    @PostMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        System.out.println("====post====");
+        messagingTemplate.convertAndSend("/topic/toUser","java");
+        return "hello send";
+    }
 
     @MessageMapping("/greeting")
     public String handle(String greeting) {
@@ -21,8 +38,14 @@ public class GreetingController {
     @MessageMapping("/say")
     @SendToUser("/queue/toUser")
     public String handleSay(String greeting){
-        System.out.println("say =>" + greeting);
-        //messageConverter.
-        return "user";
+        System.out.println("say =>" + greeting );
+        return "hello web socket!";
+    }
+
+    @MessageMapping("/say.{user}")
+    @SendToUser("/queue/toUser.{user}")
+    public String handleSayVariable(String greeting, @DestinationVariable String user){
+        System.out.println(user + "  say =>" + greeting );
+        return "hello web socket from user!";
     }
 }
